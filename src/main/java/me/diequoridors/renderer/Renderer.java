9f8b48b9cc697fe4fe32cc5bea4efab00796d806
Ferdinand Hölzig <ferdinand@it-otterstadt.de>
2024-07-03 11:28:52 +0200

    create Renderer

--- src/main/java/me/diequoridors/renderer/Renderer.java
+++ src/main/java/me/diequoridors/renderer/Renderer.java
@@ -2,12 +2,54 @@ package me.diequoridors.renderer;
 
 import me.diequoridors.world.World;
 
+import java.awt.*;
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class Renderer {
 
     private World world;
 
+    private Frame frame = new Frame();
+    private Canvas canvas = new Canvas();
+
+    private Timer frameTimer = new Timer();
+
     public Renderer(World world) {
         this.world = world;
+
+        frame.setVisible(true);
+        frame.setLayout(null);
+        frame.setResizable(true);
+        frame.setFocusable(true);
+        frame.setTitle("Quoridor");
+        frame.setSize(new Dimension(720, 720));
+
+        canvas.setVisible(true);
+        canvas.setFocusable(false);
+        canvas.setBackground(Color.WHITE);
+        canvas.setSize(frame.getSize());
+
+        frame.add(canvas);
+        canvas.createBufferStrategy(2);
+
+        frameTimer.scheduleAtFixedRate(new TimerTask() {
+            @Override
+            public void run() {
+                renderFrame();
+            }
+        }, 1000 / 60, 1000 / 60);
     }
 
+    public void exit() {
+        frameTimer.cancel();
+        frame.dispose();
+    }
+
+    private void renderFrame() {
+        Graphics graphics = canvas.getBufferStrategy().getDrawGraphics();
+        graphics.clearRect(0, 0, canvas.getWidth(), canvas.getHeight());
+
+
+    }
 }
