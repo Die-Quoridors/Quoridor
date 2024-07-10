9116248cc536f1ff1a2d075cd82a5a5355325a6f
Jab-33 <jansoundso33@gmail.com>
2024-07-10 09:06:45 +0200

    Methode getWinner

--- src/main/java/me/diequoridors/Game.java
+++ src/main/java/me/diequoridors/Game.java
@@ -11,9 +11,10 @@ public class Game {
     private final Renderer renderer;
     private final WindowInteractions windowInteractions;
     private final MouseListener mouseListener;
-
+    private final World world;
+    
     public Game(int playerCount, int wallLimit) {
-        World world = new World(wallLimit);
+        world = new World(wallLimit);
         renderer = new Renderer(world);
         windowInteractions = new WindowInteractions(renderer, this);
         mouseListener = new MouseListener(renderer, world);
@@ -28,4 +29,15 @@ public class Game {
         System.exit(0);
     }
 
+    public Player getWinnner() {
+        for (int i = 0; i < world.players.size(); i++) {
+            Player player = world.players.get(i);
+            int[] winArea = Player.playerWinArea[i];
+            if (player.x >= winArea[0] && player.y >= winArea[1] && player.x < winArea[0] + winArea[2] && player.y < winArea[1] + winArea[3]) {
+                return player;
+            }
+        }
+        
+        return null;
+    }
 }
