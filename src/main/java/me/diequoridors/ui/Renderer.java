aedd161309772c0d007478648fba81dcbdc7c202
Ferdinand Hölzig <ferdinand@it-otterstadt.de>
2024-07-11 08:26:42 +0200

    show game border and dont draw lines over border

--- src/main/java/me/diequoridors/ui/Renderer.java
+++ src/main/java/me/diequoridors/ui/Renderer.java
@@ -76,35 +76,40 @@ public class Renderer {
 
         int windowSize = Math.min(frame.getWidth(), frame.getHeight() - frame.getInsets().top);
         int cellSize = getCellSize();
-        int offsetGame = cellSize;
-        int usabelCellSize = (int) (cellSize * (1 - 2 * cellPadding));
+        int usableCellSize = (int) (cellSize * (1 - 2 * cellPadding));
         int cellOffset = (int) (cellSize * cellPadding);
+        int frameHeight = frame.getHeight() - frame.getInsets().top;
+        int frameWidth = frame.getWidth();
 
         canvas.setBounds(0, frame.getInsets().top, windowSize, windowSize);
         canvas.setSize(new Dimension(windowSize, windowSize));
 
         // === render Grid ===
-        for (int i = 1; i < gridSize; i++) {
-            graphics.setColor(Color.LIGHT_GRAY);
-            graphics.drawLine(i * cellSize + offsetGame, 0, i * cellSize + offsetGame, frame.getHeight());
-            graphics.drawLine(0, i * cellSize + offsetGame, frame.getWidth(), i * cellSize + offsetGame);
+        for (int i = 0; i <= gridSize; i++) {
+            if (i == 0 || i == gridSize) {
+                graphics.setColor(Color.BLACK);
+            } else {
+                graphics.setColor(Color.LIGHT_GRAY);
+            }
+            graphics.drawLine(i * cellSize + cellSize, cellSize, i * cellSize + cellSize, frameHeight - cellSize);
+            graphics.drawLine(cellSize, i * cellSize + cellSize, frameWidth - cellSize, i * cellSize + cellSize);
         }
 
         // === render Players ===
         for (Player player : world.players) {
-            int x = player.x * cellSize + cellOffset + offsetGame;
-            int y = player.y * cellSize + cellOffset + offsetGame;
+            int x = player.x * cellSize + cellOffset + cellSize;
+            int y = player.y * cellSize + cellOffset + cellSize;
 
             graphics.setColor(player.color);
-            graphics.fillOval(x, y, usabelCellSize, usabelCellSize);
+            graphics.fillOval(x, y, usableCellSize, usableCellSize);
             graphics.setColor(Color.BLACK);
-            graphics.drawOval(x, y, usabelCellSize, usabelCellSize);
+            graphics.drawOval(x, y, usableCellSize, usableCellSize);
         }
 
         // === render Walls ===
         for (Wall wall : world.walls) {
-            int x = wall.x * cellSize + (wall.rotation == WallRotation.Horizontal ? 0 : cellSize - (wallWidth / 2)) + offsetGame;
-            int y = wall.y * cellSize + (wall.rotation == WallRotation.Horizontal ? (cellSize - (wallWidth / 2)) : 0) + offsetGame;
+            int x = wall.x * cellSize + (wall.rotation == WallRotation.Horizontal ? 0 : cellSize - (wallWidth / 2)) + cellSize;
+            int y = wall.y * cellSize + (wall.rotation == WallRotation.Horizontal ? (cellSize - (wallWidth / 2)) : 0) + cellSize;
 
             int width = wall.rotation == WallRotation.Vertical ? wallWidth : (cellSize * 2);
             int height = wall.rotation == WallRotation.Horizontal ? wallWidth : (cellSize * 2);
